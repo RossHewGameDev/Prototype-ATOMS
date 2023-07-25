8a559fba3f8d928395a875ffd18f12609821b7d6
RossHewGameDev <98534246+RossHewGameDev@users.noreply.github.com>
2023-07-25 22:59:46 +0100

    Added some comments and opened a "WeaponFire" function for a future Agent brain

--- Assets/Scripts/Combat/TestMelee.cs
+++ Assets/Scripts/Combat/TestMelee.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class TestMelee : Attack
 {
 
-    // Update is called once per frame
     void Update()
     {
+        //Debug atttack button.
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            DamageAgent(Damage);
+            WeaponFire();
         }
         
     }
@@ -18,8 +18,36 @@ public class TestMelee : Attack
 
     private void OnTriggerEnter(Collider other)
     {
-        GetAgentProperties(other.gameObject);
+
+        TargetSelect(other);
+
     }
 
 
+    /// <summary>
+    /// Selects a target that is in range. I kinda want to replace this collider method with just a list of all the
+    /// enemies and it picks the closest one to be its target.
+    /// </summary>
+    /// <param name="collider"></param>
+    private void TargetSelect(Collider collider)
+    {
+        if (collider != null)
+        {
+            GetAgentProperties(collider.gameObject);
+        }
+        else
+        {
+            GetAgentProperties(null);
+        }
+    }
+
+
+    /// <summary>
+    /// Activates the weapon and attacks the current target.
+    /// </summary>
+    public void WeaponFire()
+    {
+        DamageAgent(Damage);
+    }
 }
+ 
