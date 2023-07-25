f1b5478e15983147beb8730e41a5c57d4ef2dfa3
RossHewGameDev <98534246+RossHewGameDev@users.noreply.github.com>
2023-07-25 17:21:02 +0100

    Updated health bar tracking units.

--- Assets/Scripts/UI Scripts/UICommands.cs
+++ Assets/Scripts/UI Scripts/UICommands.cs
@@ -7,9 +7,17 @@ public class UICommands : MonoBehaviour
 {
     public void UIFollowObject(GameObject _target, GameObject _UI, Camera _camera)
     {
-        Vector3 screenPos = _camera.WorldToScreenPoint(_target.transform.position);
+
+        Vector3 adjustedTransform = _target.transform.position;
+
+        adjustedTransform.y += 2;
+
+        Vector3 screenPos = _camera.WorldToScreenPoint(adjustedTransform);
 
         screenPos.z = 0;
+        //screenPos.y += 50;
+
+
         _UI.transform.position = screenPos;
     }
 
